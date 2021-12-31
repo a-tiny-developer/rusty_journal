46db6cdf5d14a9b7b56c4da2103672957c6b2ed7
a-tiny-flame <atinyflame@gmail.com>
2021-12-30 18:50:03 -0600

    Added function that adds taks

---
+++ src/interface.rs
@@ -0,0 +1,30 @@
+use std::fs::OpenOptions;
+use std::io::{BufReader, Result, Seek, SeekFrom};
+use std::path::PathBuf;
+
+use crate::task::Task;
+
+pub fn add_tasks(journal_path: PathBuf, task: Task) -> Result<()> {
+    // Open the file
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .open(journal_path)?;
+
+    // Consume the file's contents as a vector of tasks.
+    let mut tasks: Vec<Task> = match serde_json::from_reader(&file) {
+        Ok(tasks) => tasks,
+        Err(e) if e.is_eof() => Vec::new(),
+        Err(e) => Err(e)?,
+    };
+
+    // Rewind the file after reading from it
+    file.seek(SeekFrom::Start(0))?;
+
+    // Write the modified task list back into the file
+    tasks.push(task);
+    serde_json::to_writer(file, &tasks)?;
+
+    Ok(())
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod cli;
+pub mod interface;
 pub mod task;
